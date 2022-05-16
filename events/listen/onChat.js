15d39f18bbb8db7163087f73789cf081cda9de06
12px <d@12px.io>
2022-05-16 07:22:05 -0600

    Pass chat elem On Chat Event

--- events/listen/onChat.js
+++ events/listen/onChat.js
@@ -3,9 +3,10 @@
 
 module.exports = function (event) {
 	let self = event.senderid == this.$user.id
+	let elem = this.getChat(event.text, event.name)
 	let text = event.text, ping = this.hasPing(text)
 	let user = { userid: event.userid, name: event.name }
-	let data = { user, text, ping, self, raw: event }
+	let data = { user, text, ping, self, elem, raw: event }
 	this.Debug(`[chat] ${ user.name }`, data)
 	this.Emit("chat", data)
 }
